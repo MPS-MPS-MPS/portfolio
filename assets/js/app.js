d09b12694aac07fc68c99d7fd8f8188358ae4669
Matthew Spiller <matthewspiller1997@gmail.com>
2025-09-03 13:11:12 +1000

    email form netlify update

--- assets/js/app.js
+++ assets/js/app.js
@@ -138,7 +138,7 @@ let footer = $(`
              <div class="form-header">
                 <h6 class="display" id="get-in-touch">Get in Touch</h6>
               </div>
-                <form name="contact" netlify>
+                <form name="contact">
                   <input id="name" type="text" name="name" placeholder="Your Name" required/>
                   <input id="email" type="email" name="email" placeholder="Email Address" required/>                  
                   <textarea id="textArea" name="message" placeholder="Type your Message" required></textarea>
